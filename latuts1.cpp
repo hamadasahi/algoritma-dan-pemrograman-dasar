7761f4e07ab3de78de0b49a4e79f70a9d820f475
sa <116948898+hamadasahi@users.noreply.github.com>
2022-11-18 21:14:04 +0700

    Add files via upload

---
+++ latuts1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+main()
+{
+    int panjang,lebar,luas,keliling;
+
+    system("cls");
+    printf("Menghitung panjang dan lebar persegi panjang");
+    printf("\n\npanjang persegi panjang : ");
+    scanf("%d",&panjang);
+    printf("lebar persegi panjang : ");
+    scanf("%d",&lebar);
+
+    luas = panjang*lebar;
+    keliling = 2*panjang + 2*lebar;
+
+    printf("\nLuas persegi panjang = %d",luas);
+    printf("\nKeliling persegi panjang = %d",keliling);
+
+    getch();
+    return 0;
+}
