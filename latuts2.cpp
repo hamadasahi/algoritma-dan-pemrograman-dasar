d2bcc0f844a875ecef388f8bb48574e367e92a0d
sa <116948898+hamadasahi@users.noreply.github.com>
2022-11-18 21:14:46 +0700

    Add files via upload

---
+++ latuts2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+main()
+{
+    int celcius, fahrenheit;
+    float reamur;
+
+    system("cls");
+    printf("Konversi suhu");
+    printf("\n\nMasukkan suhu dalam satuan celcius : ");
+    scanf("%d",&celcius);
+    
+    fahrenheit=celcius+273;
+    reamur=(float)4/5*celcius+32;
+
+    printf("\nNilai suhu adalah %d derajat Fahrenheit",fahrenheit);
+    printf("\nNilai suhu adalah %.2f derajat Reamur",reamur);
+    
+    getch();
+    return 0;
+}
